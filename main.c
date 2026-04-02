e2dcd72ac5f0f355c44fc44d0a95902bf939a905
Gor Madatyan <gor-madatyan@proton.me>
2026-04-03 01:05:48 +0400

    add client server

--- main.c
+++ main.c
@@ -1,2 +1,66 @@
+#include <arpa/inet.h>
 #include <stdio.h>
-int main() { printf("aaaaa"); }
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[BUFFER_SIZE] = {0};
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    perror("Listen failed");
+    exit(EXIT_FAILURE);
+  }
+  printf("Server listening on port %d...\n", PORT);
+
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0) {
+    perror("Accept failed");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Client connected successfully!\n");
+  printf("Waiting for messages...\n\n");
+
+  while (1) {
+    memset(buffer, 0, BUFFER_SIZE);
+    int bytes_read = read(new_socket, buffer, BUFFER_SIZE - 1);
+
+    if (bytes_read <= 0) {
+      printf("\nClient disconnected or network error occurred.\n");
+      break;
+    }
+
+    printf("Client: %s", buffer);
+
+    if (strncmp(buffer, "exit", 4) == 0) {
+      printf("Server shutting down...\n");
+      break;
+    }
+  }
+
+  close(new_socket);
+  close(server_fd);
+  return 0;
+}
